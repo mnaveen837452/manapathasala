2b3ba1ec44d79473a144524b50682bad79e1c377
Naveen <naveenkumarvrp@gmai.com>
2022-04-26 17:45:50 +0530

    Added basic project structure

---
+++ src/main/java/com/manapathasala/ManapathasalaApplication.java
@@ -0,0 +1,13 @@
+package com.manapathasala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManapathasalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManapathasalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manapathasala/ManapathasalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manapathasala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManapathasalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
